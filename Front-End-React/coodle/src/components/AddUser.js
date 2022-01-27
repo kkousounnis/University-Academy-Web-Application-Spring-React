58289e258abc2e485bb638aba1db90722eca15e6
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-01-27 01:56:33 +0100

    Remove unused files add new html.

--- Front-End-React/coodle/src/components/AddUser.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const AddUser = () => {
-  return <div>
-      <h1>AddUser</h1>
-  </div>;
-};
--- Front-End-React/coodle/src/components/EditUser.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const EditUser = () => {
-  return <div>
-      <h1>Edit User</h1>
-  </div>;
-};
