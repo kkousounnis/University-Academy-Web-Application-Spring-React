653668c662d497bb073bc51e3b7bc8f999286afc
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-02-07 20:34:28 -0700

    Fixed issue in the Back End Authorization JWT.

--- Back-End-SpringBoot/Coodle/src/main/java/com/spring/boot/coodle/config/WebSecurityConfig.java
+++ Back-End-SpringBoot/Coodle/src/main/java/com/spring/boot/coodle/config/WebSecurityConfig.java
@@ -17,12 +17,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-
-
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(
-        // securedEnabled = true,
+        //securedEnabled = true,
         // jsr250Enabled = true,
         prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
--- Back-End-SpringBoot/Coodle/src/main/java/com/spring/boot/coodle/config/security/jwt/AuthTokenFilter.java
+++ Back-End-SpringBoot/Coodle/src/main/java/com/spring/boot/coodle/config/security/jwt/AuthTokenFilter.java
@@ -52,7 +52,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 	private String parseJwt(HttpServletRequest request) {
 		String headerAuth = request.getHeader("Authorization");
 
-		if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+		if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer")) {
 			return headerAuth.substring(7, headerAuth.length());
 		}
 
