20ec1fa13f3e7f6d6b768abe2e1fb73f0219968a
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-02-08 05:51:24 +0200

    Deleted files

--- Back-End-SpringBoot/Coodle/src/main/java/com/spring/boot/coodle/entities/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.spring.boot.coodle.entities;
-
-public enum ERole {
-    ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
-}
--- Front-End-React/coodle/src/services/auth-header.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader() {
-  const user = JSON.parse(localStorage.getItem('user'));
-
-  if (user && user.accessToken) {
-    return { 'Authorization': 'Bearer ' + user.accessToken }; // for Spring Boot back-end
-  } else {
-    return {};
-  }
-}
