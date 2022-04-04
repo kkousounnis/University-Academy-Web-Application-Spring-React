c7548b3100d5281bab909be925d6259c2b6ee046
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-04-05 02:57:53 +1200

    Updated table/class trainer.

---
+++ Back-End-SpringBoot-Server/Coodle/src/main/java/com/spring/boot/coodle/entities/Trainer.java
@@ -0,0 +1,96 @@
+
+package com.spring.boot.coodle.entities;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "trainers")
+public class Trainer {
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "subbject", nullable = false, length = 255)
+    private String subbject;
+    
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "trainers_courses",
+            joinColumns = @JoinColumn(name = "trainer_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id"))
+    private Set<Trainer> trainers = new HashSet<>();  
+    
+    public Trainer() {
+    }
+
+    public Trainer(Integer id, String subbject, User user) {
+        this.id = id;
+        this.subbject = subbject;
+        this.user = user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSubbject() {
+        return subbject;
+    }
+
+    public void setSubbject(String subbject) {
+        this.subbject = subbject;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Trainer{");
+        sb.append("id=").append(id);
+        sb.append(", subbject=").append(subbject);
+        sb.append(", user=").append(user);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+}
