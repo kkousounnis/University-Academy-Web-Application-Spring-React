432e82c91cd3b0bbd5c3ffbd967dfa86adb0ef09
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-05-30 23:36:00 +0300

    Update courses.

--- Back-End-SpringBoot-Server/Coodle/src/main/java/com/spring/boot/coodle/entities/Course.java
+++ Back-End-SpringBoot-Server/Coodle/src/main/java/com/spring/boot/coodle/entities/Course.java
@@ -1,9 +1,8 @@
-
 package com.spring.boot.coodle.entities;
 
-import java.util.Date;
 import java.util.List;
 import javax.persistence.Basic;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -14,6 +13,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -21,40 +22,43 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name = "courses")
 public class Course {
+
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "id", nullable = false)
     private Integer id;
-    
+
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 255)
     @Column(name = "title", nullable = false, length = 255)
     private String title;
-    
+
     @Enumerated(EnumType.STRING)
     @Column(name = "semester", length = 255)
     private ESemester semester;
-    
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 255)
-    @Column(name = "assignment", nullable = false, length = 255)
-    private String assignment;
-    
-    @Basic(optional = false)
-    @NotNull
-    @Column(name = "submission_date", length = 255)
-    private Date submissionDate;
-    
+
+    @Column(name = "price", nullable = false)
+    private Integer price;
+
+    @Column(name = "id_paid", columnDefinition = "boolean default false")
+    private Boolean isPaid;
+
+    @Column(name = "has_attended_e_courses", columnDefinition = "boolean default false")
+    private Boolean hasAttendedECourses;
+
+    @OneToOne(mappedBy = "course", cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn
+    private Assignment assignment;
+
     @JoinTable(name = "students_courses", joinColumns = {
         @JoinColumn(name = "course_id", referencedColumnName = "id", nullable = false)}, inverseJoinColumns = {
         @JoinColumn(name = "student_id", referencedColumnName = "user_id", nullable = false)})
     @ManyToMany
     private List<Student> studentList;
-    
+
     @JoinTable(name = "trainers_courses", joinColumns = {
         @JoinColumn(name = "course_id", referencedColumnName = "id", nullable = false)}, inverseJoinColumns = {
         @JoinColumn(name = "trainer_id", referencedColumnName = "user_id", nullable = false)})
@@ -64,12 +68,20 @@ public class Course {
     public Course() {
     }
 
-    public Course(Integer id, String title, ESemester semester, String assignment, Date submissionDate) {
+    public Course(Integer id, String title, ESemester semester) {
         this.id = id;
         this.title = title;
         this.semester = semester;
-        this.assignment = assignment;
-        this.submissionDate = submissionDate;
+    }
+
+    public Course(Integer id, String title, ESemester semester, Integer Price,
+            Boolean isPaid, Boolean hasAttendedECourses) {
+        this.id = id;
+        this.title = title;
+        this.semester = semester;
+        this.price = Price;
+        this.isPaid = isPaid;
+        this.hasAttendedECourses = hasAttendedECourses;
     }
 
     public Integer getId() {
@@ -96,20 +108,36 @@ public class Course {
         this.semester = semester;
     }
 
-    public String getAssignment() {
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer Price) {
+        this.price = Price;
+    }
+
+    public Boolean getIsPaid() {
+        return isPaid;
+    }
+
+    public void setIsPaid(Boolean isPaid) {
+        this.isPaid = isPaid;
+    }
+
+    public Boolean getHasAttendedECourses() {
+        return hasAttendedECourses;
+    }
+
+    public void setHasAttendedECourses(Boolean hasAttendedECourses) {
+        this.hasAttendedECourses = hasAttendedECourses;
+    }
+
+    public Assignment getAssignment() {
         return assignment;
     }
 
-    public void setAssignment(String assignment) {
+    public void setAssignment(Assignment assignment) {
         this.assignment = assignment;
-    }
-
-    public Date getSubmissionDate() {
-        return submissionDate;
-    }
-
-    public void setSubmissionDate(Date submissionDate) {
-        this.submissionDate = submissionDate;
     }
 
     @Override
@@ -119,10 +147,8 @@ public class Course {
         sb.append("id=").append(id);
         sb.append(", title=").append(title);
         sb.append(", semester=").append(semester);
-        sb.append(", assignment=").append(assignment);
-        sb.append(", submissionDate=").append(submissionDate);
         sb.append('}');
         return sb.toString();
     }
-     
+
 }
