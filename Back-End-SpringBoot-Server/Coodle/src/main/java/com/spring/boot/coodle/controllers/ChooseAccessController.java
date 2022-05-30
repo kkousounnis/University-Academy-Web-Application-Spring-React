74848d4a7c03272068541a1b44cd697168a1a57d
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-05-30 23:15:28 +0300

    Updated test controller to choose controller.

--- Back-End-SpringBoot-Server/Coodle/src/main/java/com/spring/boot/coodle/controllers/ChooseAccessController.java
+++ Back-End-SpringBoot-Server/Coodle/src/main/java/com/spring/boot/coodle/controllers/ChooseAccessController.java
@@ -1,5 +1,7 @@
 package com.spring.boot.coodle.controllers;
 
+import com.spring.boot.coodle.entities.dto.responses.MessageResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/test")
-public class TestController {
+@RequestMapping("/api/choose-access")
+public class ChooseAccessController {
 	@GetMapping("/all")
 	public String allAccess() {
 		return "Learn without limits";
@@ -17,8 +19,10 @@ public class TestController {
 	
 	@GetMapping("/user")
 	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
-	public String userAccess() {
-		return "User Content.";
+	public ResponseEntity<?> userAccess() {
+            
+                
+                return (ResponseEntity.ok(new MessageResponse("User Content.")));
 	}
 
 	@GetMapping("/mod")
