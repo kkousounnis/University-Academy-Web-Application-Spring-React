236fa373b4de9c3c49198b9eb6a7ff24829ebf86
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-06-07 08:10:09 -0500

    Updated user response table.

--- Back-End-SpringBoot-Server/Coodle/src/main/java/com/spring/boot/coodle/entities/dto/responses/UserResponseTable.java
+++ Back-End-SpringBoot-Server/Coodle/src/main/java/com/spring/boot/coodle/entities/dto/responses/UserResponseTable.java
@@ -1,33 +1,46 @@
-
 package com.spring.boot.coodle.entities.dto.responses;
 
 import com.spring.boot.coodle.entities.Assignment;
 import com.spring.boot.coodle.entities.Course;
+import java.util.List;
 
 public class UserResponseTable {
-    private Course course;
-      
-    private Assignment assignment;
 
-    public UserResponseTable(Course course, Assignment assignment) {
+    private List<Course> course;
+
+    private List<Assignment> assignment;
+
+    public UserResponseTable() {
+    }
+
+    public UserResponseTable(List<Course> course, List<Assignment> assignment) {
         this.course = course;
         this.assignment = assignment;
     }
 
-    public Course getCourse() {
+    public List<Course> getCourse() {
         return course;
     }
 
-    public void setCourse(Course course) {
+    public void setCourse(List<Course> course) {
         this.course = course;
     }
 
-    public Assignment getAssignment() {
+    public List<Assignment> getAssignment() {
         return assignment;
     }
 
-    public void setAssignment(Assignment assignment) {
+    public void setAssignment(List<Assignment> assignment) {
         this.assignment = assignment;
     }
-    
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("course=").append(course);
+        sb.append(", assignment=").append(assignment);
+        sb.append('}');
+        return sb.toString();
+    }
+
 }
