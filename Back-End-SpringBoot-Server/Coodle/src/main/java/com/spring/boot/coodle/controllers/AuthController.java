314cb4119680d5c1306c00af511f9871641caf46
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-06-28 00:19:41 -0700

    Updaed auth controller.

--- Back-End-SpringBoot-Server/Coodle/src/main/java/com/spring/boot/coodle/controllers/AuthController.java
+++ Back-End-SpringBoot-Server/Coodle/src/main/java/com/spring/boot/coodle/controllers/AuthController.java
@@ -104,7 +104,9 @@ public class AuthController {
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-
+         System.err.println("Check from here");
+        System.err.println("Check from here"+ signUpRequest);
+        
         if (userRepository.existsByEmail(signUpRequest.getEmail())) {
             return ResponseEntity
                     .badRequest()
@@ -146,11 +148,11 @@ public class AuthController {
         }
 
         user.setRoles(roles);
-
+        System.err.println("Check from here"+ user);
         userRepository.save(user);
         //to be checked and implemented
-//        return new ResponseEntity<MessageResponse>(new MessageResponse("User registered successfully!"), HttpStatus.CREATED);
-        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+        return new ResponseEntity(new MessageResponse("User registered successfully!"),HttpStatus.CREATED);
+//        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
     }
 
     @PostMapping("/forgot-password")
